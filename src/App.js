8c40191c4652be9995d8018dad9e1db047d90853
anjaaleksa <marjan1505@gmail.com>
2022-10-19 18:26:52 +0200

    useEffect and button start stop count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import { useEffect } from "react";
+import React from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  console.log("render from App")
+  const [count, setCount] = useState(0)
+  const [isdisable, setIsdisable] = useState(true)
+  useEffect(() => {
+    if (!isdisable) {
+      setTimeout(() => {
+        setCount((count) => count + 1)
+      }, 1000)
+    } else {
+      setCount(count)
+    }
+  })
+  const changeStatus = (e) => {
+    e.preventDefault()
+
+    if (e.currentTarget.textContent === 'Start') {
+      setIsdisable(false)
+    } else {
+      setIsdisable(true)
+    }
+  }
+
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <button onClick={changeStatus}>Start</button>
+      <button onClick={changeStatus}>Stop</button>
+      <h1>Count: {count}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
